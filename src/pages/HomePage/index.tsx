d277649a01161e3f91c5484fe21694efdfc5cbab
lengyifan.evan <lengyifan.evan@bytedance.com>
2021-09-20 17:16:55 +0800

    feat(broccoli_and_company_web): 初始化broccoli_and_company的页面

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+    return <div>这里是首页的内容</div>
+}
+
+export default HomePage
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import HomePage from '../pages/HomePage';
+
+const routes = [
+    {
+      path: "/",
+      exact: true,
+      title: "首页",
+      main: () => <HomePage />
+    },
+  ];
+
+  export default routes;
